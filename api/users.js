511e98ae9953df19c5e693a915b24b73e04d5b75
GIT-DS <darrickshin@gmail.com>
2021-10-10 16:08:08 -0700

    Setup backend User Auth

---
+++ api/users.js
@@ -0,0 +1,105 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+const passport = require('passport');
+const validateRegisterInput = require('../../validation/register.js');
+const validateLoginInput = require('../../validation/login.js');
+
+router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
+
+router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  User.findOne({ handle: req.body.handle }).then(user => {
+    if (user) {
+      errors.handle = "User already exists";
+      return res.status(400).json(errors);
+    } else {
+      const newUser = new User({
+        handle: req.body.handle,
+        email: req.body.email,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => {
+              const payload = { id: user.id, handle: user.handle };
+
+              jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
+                res.json({
+                  success: true,
+                  token: "Bearer " + token
+                });
+              });
+            })
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+});
+
+router.post('/login', (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({email})
+    .then(user => {
+      if (!user) {
+        return res.status(404).json({email: 'This user does not exist'});
+      }
+
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {id: user.id, handle: user.handle};
+          
+            jwt.sign(
+              payload,
+              keys.secretOrKey,
+              // Tell the key to expire in one hour
+              {expiresIn: 3600},
+              (err, token) => {
+                res.json({
+                  success: true,
+                  token: 'Bearer ' + token
+                });
+              });
+          } else {
+            return res.status(400).json({password: 'Incorrect password'});
+          }
+        })
+    })
+})
+
+
+router.get('/current', passport.authenticate('jwt', {session: false}), (req, res) => {
+  res.json({
+    id: req.user.id,
+    handle: req.user.handle,
+    email: req.user.email
+  });
+})
+
+
+
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const path = require('path')
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+require('./config/passport')(passport);
+
+const port = process.env.PORT || 5000;
+
+
+app.use("/api/users", users);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
