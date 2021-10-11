c8fba664aaa2b7655199f2b53e79729e021cc6c9
GIT-DS <darrickshin@gmail.com>
2021-10-11 10:52:11 -0700

    Add category validation

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,42 +1,42 @@
-// const express = require("express");
-// const router = express.Router();
-// const mongoose = require('mongoose')
-// const passport = require('passport')
+const express = require("express");
+const router = express.Router();
+const mongoose = require('mongoose')
+const passport = require('passport')
 
-// const  Category = require('../../models/Category')
-// const validateCategoryInput = require('../../validation/Category')
+const  Category = require('../../models/Category')
+const validateCategoryInput = require('../../validation/category')
 
-// router.get("/test", (req, res) => res.json({ msg: "This is the categories route" }));
+router.get("/test", (req, res) => res.json({ msg: "This is the categories route" }));
 
-// router.get('/', (req, res) => {
-//     Category.find()
-//         .then(categories => res.json(categories))
-//         .catch( err => res.status(404).json({ nocategoriesfound: 'No categories found' }))
-// });
+router.get('/', (req, res) => {
+    Category.find()
+        .then(categories => res.json(categories))
+        .catch( err => res.status(404).json({ nocategoriesfound: 'No categories found' }))
+});
 
-// router.get('/:id', (req, res) => {
-//     Category.findById(req.params.id)
-//         .then(category => res.json(category))
-//         .catch( err => res.status(404).json( { nocategoryfound: 'No Category found with that ID'} ) )
-// });
+router.get('/:id', (req, res) => {
+    Category.findById(req.params.id)
+        .then(category => res.json(category))
+        .catch( err => res.status(404).json( { nocategoryfound: 'No Category found with that ID'} ) )
+});
 
-// router.post('/', 
-//     passport.authenticate('jwt', { session: false }),
-//     (req, res) => {
-//         const {errors, isValid} = validateCategoryInput(req.body);
-//         if (!isValid){
-//             return res.status(400).json(errors)
-//         }
+router.post('/', 
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const {errors, isValid} = validateCategoryInput(req.body);
+        if (!isValid){
+            return res.status(400).json(errors)
+        }
 
-//         const newCategory = new Category({
-//             name: req.body.name,
-//             recipes: req.user.recipes
-//         });
+        const newCategory = new Category({
+            name: req.body.name,
+            recipes: req.user.recipes
+        });
 
-//         newCategory.save().then( category => res.json(category))
-//     }
-// );
+        newCategory.save().then( category => res.json(category))
+    }
+);
 
 
 
-// module.exports = router;
+module.exports = router;
---
+++ validation/category.js
@@ -0,0 +1,21 @@
+const Validator = require('validator')
+const validText = require('./valid-text')
+
+module.exports = function validateCategoryInput(data){
+    let errors = {}
+    data.name = validText(data.name) ? data.name : ''
+    if (!Validator.isLength(data.text, {min: 2, max: 140})){
+        errors.name = 'Category name must be between 2 and 140 characters'
+    }
+
+    if (Validator.isEmpty(data.name)) {
+        errors.name = 'Name field is required'
+    }
+
+    if (data.recipes.length === 0) errors.recipes = 'At least one recipe is required'
+
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0
+    }
+}
