70a00a7c6975e29e4c1d26ee2c6963a9245c26a6
David Oh <davidohyoung@gmail.com>
2021-10-12 13:24:01 -0500

    merge main to show

---
+++ frontend/src/components/recipes/recipe_show.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+
+export default class RecipeShow extends Component {
+    render() {
+        return (
+            <div className="show-container">
+                <div className="show-header">
+                    <div className="recipe-picture">
+
+                    </div>
+                    <div className="recipe-info">
+
+                    </div>
+                </div>
+                <div className="show-content">
+                    <div className="show-ingredients">
+
+                    </div>
+                    <div className="show-directions">
+
+                    </div>
+                </div>
+                <div className="show-reviews">
+                    
+                </div>
+            </div>
+        )
+    }
+}
