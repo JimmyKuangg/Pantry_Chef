e2d1f1d52fcd0245912808e7aaed080265d8f363
GIT-DS <darrickshin@gmail.com>
2021-10-12 11:43:10 -0700

    Add fetchAllRecipes

--- frontend/src/util/recipe_util.js
+++ frontend/src/util/recipe_util.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const fetchAllRecipes = () => {
+  return axios.get('/api/recipes')
+}
+
 export const fetchRecipe = (recipeId) => {
   return axios.get(`/api/recipes/${recipeId}`);
 };
