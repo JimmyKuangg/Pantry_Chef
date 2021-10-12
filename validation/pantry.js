48f1c08b5089e7230ee8e3572862ef0fd208730f
Jimmy Kuang <jimmy.kuang96@gmail.com>
2021-10-12 10:04:22 -0700

    Finished pantry validation

--- validation/pantry.js
+++ validation/pantry.js
@@ -2,3 +2,23 @@ const Validator = require('validator');
 const validText = require('./valid-text');
 const validArray = require('./valid-array');
 
+module.exports = function validatePantryInput(data) {
+  let errors = {};
+
+  data.ingredients = validArray(data.ingredients) ? data.ingredients : '';
+  for(let i = 0; i < data.ingredients.length; i++) { 
+    if (!Validator.isNumeric(data.ingredients.quantity)) {
+      errors.ingredients.quantity = 'Quantity is required';
+    }
+
+    data.ingredients.unit = validText(data.ingredients.unit) ? data.ingredients.unit : '';
+    if (Validator.isEmpty(data.ingredients.unit)) {
+      errors.ingredients.unit = 'Unit of measure is required';
+    }
+  }
+
+  return {
+    errors,
+    isValid: Object.keys(errors).length === 0
+  }
+}
