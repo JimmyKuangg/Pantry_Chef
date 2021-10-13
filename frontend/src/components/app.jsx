48dffeed8510002f6ff5ac4054794dc041a4c355
imvincenth <vincesuuu1@gmail.com>
2021-10-12 17:37:41 -0700

    Placed modal inside of main app

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import { AuthRoute } from "../util/route_util";
 import { Route } from "react-router";
 import { Switch } from "react-router-dom";
+import Modal from './modal/modal';
 import NavBarContainer from "./nav/navbar_container";
-import RecipeShowContainer from './recipes/recipe_show_container'
+import RecipeShowContainer from './recipes/recipe_show_container';
+import PantryShowContainer from './pantry/pantry_container';
 
 import MainPage from "./main/main_page";
 // import LoginFormContainer from "./session/login_form_container";
@@ -12,8 +14,10 @@ import MainPage from "./main/main_page";
 const App = () => (
   <div>
     <NavBarContainer />
+    <Modal />
     <Switch>
       <Route exact path='/recipes/:recipeId' component={RecipeShowContainer} />
+      <Route exact path='/pantries' component={PantryShowContainer} />
       <AuthRoute exact path="/" component={MainPage} />
       {/* <AuthRoute exact path="/login" component={LoginFormContainer} />
       <AuthRoute exact path="/signup" component={SignupFormContainer} /> */}
--- frontend/src/components/pantry/pantry.jsx
+++ frontend/src/components/pantry/pantry.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Pantry extends Component {
+  
+
   render() {
     return (
       <div>
-        
+        Check the logs
+        {console.log(this.props.pantries)}
       </div>
     )
   }
