8e150aee69ccbb2cb3b585fdf3f032c2252b1793
David Oh <davidohyoung@gmail.com>
2021-10-12 19:37:11 -0500

    setup index container

--- frontend/src/components/recipes/recipe_index_container.js
+++ frontend/src/components/recipes/recipe_index_container.js
@@ -1,12 +1,16 @@
-import { connect } from 'react-redux';
-import RecipeIndex from './recipe_index';
+import { connect } from "react-redux";
+import RecipeIndex from "./recipe_index";
+import { fetchRecipes } from "../../actions/recipe_actions";
 
-const mSTP = state => ({
+const mSTP = (state) => {
+    console.log(state)
+    return {
+        recipes: state.recipes,
+    }
+};
 
-});
-
-const mDTP = dispatch => ({
-
+const mDTP = (dispatch) => ({
+  fetchRecipes: () => dispatch(fetchRecipes()),
 });
 
 export default connect(mSTP, mDTP)(RecipeIndex);
