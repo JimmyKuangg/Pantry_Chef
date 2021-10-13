a780cd187a9d83975fbc32960f868c7d44ce557d
David Oh <davidohyoung@gmail.com>
2021-10-12 20:36:54 -0500

    new index branch

--- frontend/src/components/recipes/recipe_index.jsx
+++ frontend/src/components/recipes/recipe_index.jsx
@@ -1,15 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class RecipeIndex extends Component {
-  render() {
-    return (
-      <div className='index-wrapper'>
-        <div className='search-wrapper'>
-          {/* <div className=''>
+  componentDidMount() {
+    this.props.fetchRecipes();
+    console.log(this.props.state)
+  }
 
-          </div> */}
+  render() {
+    const { recipes } = this.props;
+
+    if (recipes === undefined) {
+      return null;
+    }
+
+    return (
+      <div className="index-page">
+        <div className="index-main">
+          <div className="index-logo">Logo</div>
+          <div className="index-search-bar">Search Bar</div>
+          <div className='index-recipes'>
+            <ul>
+              {
+                recipes.map( (recipe, i) => {
+                  <li key={i} className='index-recipe-card'>
+                    {recipe.name}
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+        </div>
+        <div className="index-sidebar">
+          <div className="index-sidebar-ingredients">
+            <ul>
+              <li>Ingredient</li>
+              <li>Ingredient</li>
+              <li>Ingredient</li>
+            </ul>
+          </div>
+          <button className="save-ingredients">
+            Save Ingredients to My Pantry
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
--- frontend/src/components/recipes/recipe_index_container.js
+++ frontend/src/components/recipes/recipe_index_container.js
@@ -1,12 +1,17 @@
-import { connect } from 'react-redux';
-import RecipeIndex from './recipe_index';
+import { connect } from "react-redux";
+import RecipeIndex from "./recipe_index";
+import { fetchRecipes } from "../../actions/recipe_actions";
 
-const mSTP = state => ({
+const mSTP = (state) => {
+    console.log(state)
+    return {
+        recipes: Object.values(state.recipes),
+        state: state
+    }
+};
 
-});
-
-const mDTP = dispatch => ({
-
+const mDTP = (dispatch) => ({
+  fetchRecipes: () => dispatch(fetchRecipes()),
 });
 
 export default connect(mSTP, mDTP)(RecipeIndex);
