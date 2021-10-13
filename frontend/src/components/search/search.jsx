e85353c80763f6cd308635339c1948f984313273
GIT-DS <darrickshin@gmail.com>
2021-10-12 19:21:25 -0700

    Add Search

--- frontend/src/components/search/search.jsx
+++ frontend/src/components/search/search.jsx
@@ -2,19 +2,73 @@ import React, { Component } from 'react'
 import "./search.css";
 
 export default class Search extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      search: '',
+      ingredientSuggestions: [],
+      selectedIngredients: []
+    }
+    this.updateSuggestion = this.updateSuggestion.bind(this)
+    this.suggestionClickHandler = this.suggestionClickHandler.bind(this)
+    this.removeSelectedClickHandler = this.removeSelectedClickHandler.bind(this)
+  }
+
+  componentDidMount(){
+    this.props.fetchAllIngredients()
+  }  
+
+  update(){
+    return e => this.setState({search: e.currentTarget.value})
+  }
+
+  updateSuggestions(){
+    let suggestions = this.props.ingredients.filter(ingredient => 
+      !selectedIngredients.includes(ingredient) &&
+      ingredient.name.includes(this.state.search))
+    this.setState({ingredientSuggestions: suggestions})
+  }
+
+  suggestionClickHandler(e){
+    let newSelectedIngredients = this.state.selectedIngredients;
+    newSelectedIngredients.push(e.currentTarget.value)
+    this.setState({selectedIngredients: newSelectedIngredients})
+  }
+
+  removeSelectedClickHandler(e){
+    let newSelectedIngredients = this.state.selectedIngredients.filter(ingredient => ingredient !== e.currentTarget.value)
+    this.setState({selectedIngredients: newSelectedIngredients})
+  }
+
+
   render() {
+
+  
+
     return (
       <div>
         <div className="search-wrapper">
 
           <div className="search-input-wrapper">
-            <input type="text" className="search-bar" placeholder="Enter an ingredient..." />
+            <input type="text" 
+              className="search-bar" 
+              value={this.state.serach} 
+              placeholder="Enter an ingredient..."
+              onChange={this.update()}
+            />
             
             <div className="search-icon"></div>
           </div>
-
+          <div className='ingredient-suggestions'>
+            <ul>
+              {this.state.ingredientSuggestions.map((suggestion, i) => <li key={i} onClick={this.suggestionClickHandler}>{suggestion}</li>)}
+            </ul>      
+          </div>
           <div className="selected-ingredients">
             Ingredients
+            <ul>
+              {this.state.selectedIngredients.map((ingredient,i) => <li key={i} onClick={this.removeSelectedClickHandler}>{ingredient}</li>)}
+            </ul>
           </div>
     
         </div>
--- frontend/src/components/search/search_container.js
+++ frontend/src/components/search/search_container.js
@@ -1,12 +1,16 @@
 import { connect } from 'react-redux';
 import SearchBar from './search';
+import { fetchRecipes } from '../../actions/recipe_actions';
+import { fetchAllIngredients } from '../../actions/ingredient_actions';
 
 const mSTP = state => ({
-
+    ingredients: Object.values(state.ingredients),
+    recipes: state.recipes
 });
 
 const mDTP = dispatch => ({
-
+    fetchRecipes: () => dispatch( fetchRecipes () ),
+    fetchAllIngredients: () => dispatch( fetchAllIngredients() )
 });
 
 export default connect(mSTP, mDTP)(SearchBar);
