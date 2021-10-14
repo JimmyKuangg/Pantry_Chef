7de7e8be7a3579cece410a1e188b6443ee841fb2
Jimmy Kuang <jimmy.kuang96@gmail.com>
2021-10-14 13:23:48 -0700

    Working on bugs with pantry updating and validating

--- frontend/src/components/pantry/pantry.jsx
+++ frontend/src/components/pantry/pantry.jsx
@@ -4,10 +4,21 @@ export default class Pantry extends Component {
   constructor(props){
     super(props);
 
-    this.state = this.props.pantry;
+    this.state = {
+      ingredients: this.props.pantry.ingredients,
+      user: this.props.pantry.user,
+      showSelect: false,
+      notInPantry: [],
+      // selectedIngredientId: '',
+      selectedIngredient: '',
+      selectedQuantity: '',
+      selectedUnit: '',
+    }
     this.removePantryItem = this.removePantryItem.bind(this);
     this.updatePantry = this.updatePantry.bind(this);
     this.addToPantry = this.addToPantry.bind(this);
+    this.updateCurrentPantry = this.updateCurrentPantry.bind(this);
+    this.updateField = this.updateField.bind(this);
   }
 
   componentDidMount(){
@@ -27,11 +38,11 @@ export default class Pantry extends Component {
       inPantry.push(this.state.ingredients[i].ingredient);
     }
     
-    let notInPantry = this.props.ingredients.filter(ingredient => (
+    this.setState({notInPantry: this.props.ingredients.filter(ingredient => (
       !inPantry.includes(ingredient.name)
-    ));
+    ))})
 
-    console.log(notInPantry);
+    this.setState({showSelect: true});
   }
 
   removePantryItem(e, itemId) {
@@ -41,14 +52,32 @@ export default class Pantry extends Component {
   }
   
   updatePantry() {
-    this.props.editPantry(this.state);
+    this.props.editPantry({ingredients: this.state.ingredients});
+  }
+
+  updateField(field) {
+    return e => {
+      console.log(this.state);
+      this.setState({ [field]: e.target.value })
+    }
+  }
+
+  updateCurrentPantry() {
+    let newPantryIngredients = this.state.ingredients
+    newPantryIngredients.push({
+      ingredient: this.state.selectedIngredient,
+      quantity: this.state.selectedQuantity,
+      unit: this.state.selectedUnit
+    });
+
+    this.setState({ingredients: newPantryIngredients})
   }
 
   render() {
     if (!this.props.pantry.ingredients) {
       return null;
     }
-
+    console.log(this.state.ingredients);
     return (
       <div>
         My Pantry
@@ -60,7 +89,18 @@ export default class Pantry extends Component {
             </li>
           )) : ''}
         </ul>
-        <button onClick={() => this.addToPantry()}>Add things to your pantry!</button>
+        { this.state.showSelect ? 
+        <form onSubmit={this.updateCurrentPantry}>
+          <select className="ingredients-select-box" onChange={this.updateField('selectedIngredient') }>
+            <option value="none" selected disabled hidden/>
+            {this.state.notInPantry.map((ingredient, i) => <option key={i} value={ingredient._id}>{ingredient.name}</option>)}
+          </select> 
+          <input type="text" placeholder="QUANTITY" onChange={this.updateField('selectedQuantity')}/>
+          <input type="text" placeholder="UNIT" onChange={this.updateField('selectedUnit')}/>
+          <button type="submit">Add to your current pantry</button>
+        </form>
+        : ''}
+        <button onClick={() => this.addToPantry()}>Add items to your pantry</button>
         <button onClick={() => this.updatePantry()}>Save your pantry</button>
       </div>
     )
