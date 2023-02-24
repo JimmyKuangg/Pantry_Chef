a3835013d7811b911f6b5129a06284a9e4373068
Jimmy Kuang <jimmy.kuang96@gmail.com>
2023-02-23 22:15:09 -0800

    Pushing to heroku

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const db = require('./config/keys').mongoURI;
-const users = require("./routes/api/users");
+const users = require('./routes/api/users');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const recipes = require("./routes/api/recipes");
-const ingredients = require("./routes/api/ingredients");
-const pantries = require("./routes/api/pantries");
-const categories = require("./routes/api/categories")
+const recipes = require('./routes/api/recipes');
+const ingredients = require('./routes/api/ingredients');
+const pantries = require('./routes/api/pantries');
+const categories = require('./routes/api/categories');
 
-const path = require('path')
+const path = require('path');
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
   app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+  });
 }
 
 mongoose
   .connect(db, { useNewUrlParser: true })
-  .then(() => console.log("Connected to MongoDB successfully"))
+  .then(() => console.log('Connected to MongoDB successfully'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,10 +29,10 @@ require('./config/passport')(passport);
 
 const port = process.env.PORT || 5000;
 
-app.use("/api/users", users);
-app.use("/api/recipes", recipes);
-app.use("/api/ingredients", ingredients);
-app.use('/api/pantries', pantries)
-app.use('/api/categories', categories)
+app.use('/api/users', users);
+app.use('/api/recipes', recipes);
+app.use('/api/ingredients', ingredients);
+app.use('/api/pantries', pantries);
+app.use('/api/categories', categories);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
